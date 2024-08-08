508eae6e96784911ef8f720cb024547d8eac86b9
Risbel Suarez <risbel961019@gmail.com>
2024-08-08 01:31:33 -0400

    Replace footer icons

--- src/app/[lang]/components/sections/footer/Footer.tsx
+++ src/app/[lang]/components/sections/footer/Footer.tsx
@@ -1,22 +1,23 @@
 import { Facebook, InstagramIcon, Mail, MapPin, Phone } from "lucide-react";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <footer id="contact" className="flex flex-col gap-4 items-center justify-center bg-black text-white p-4 h-[50vh]">
       <div className="flex items-center justify-center space-x-4">
         <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/profile.php?id=61563395220882">
-          <Facebook className="text-white h-6 w-6" />
+          <Image src={"/facebook.svg"} alt="icon " height={30} width={30} />
         </a>
 
         <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/aricocaveexperience/">
-          <InstagramIcon className="text-white h-6 w-6" />
+          <Image src={"/instagram-logo.svg"} alt="icon " height={30} width={30} />
         </a>
 
         <a target="_blank" rel="noopener noreferrer" href="mailto:arlet@aricocave.com">
-          <Mail className="text-white h-6 w-6" />
+          <Image src={"/mail.svg"} alt="icon " height={30} width={30} />
         </a>
         <a target="_blank" rel="noopener noreferrer" href="tel:+34 672 10 44 21">
-          <Phone className="text-white h-6 w-6" />
+          <Image src={"/phone.svg"} alt="icon " height={30} width={30} />
         </a>
       </div>
       <div className="flex flex-col items-center">
@@ -26,7 +27,7 @@ const Footer = () => {
           rel="noopener noreferrer"
           href="https://www.facebook.com/profile.php?id=61563395220882"
         >
-          <MapPin className="text-white h-6 w-6" />
+          <Image src={"/map-with-location.svg"} alt="icon " height={30} width={30} />
           <span className="font-light text-xs"> Morra del Pino N°9, El Bueno, Arico, Canarias, Spain</span>
         </a>
         <a target="_blank" rel="noopener noreferrer" href="https://www.aricocave.com">
