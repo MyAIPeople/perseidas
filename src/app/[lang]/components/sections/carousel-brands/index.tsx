f1c5a1b7d69d25a96b3d536c0d1b2a745628c043
Risbel Suarez <risbel961019@gmail.com>
2024-08-08 14:05:07 -0400

    feat: Add some banner images

--- src/app/[lang]/components/sections/carousel-brands/index.tsx
+++ src/app/[lang]/components/sections/carousel-brands/index.tsx
@@ -5,7 +5,7 @@ const CarouselBrands = () => {
   const carouselBrands = useCarouselBrands();
 
   return (
-    <section className="bg-slate-950 w-full inline-flex flex-nowrap overflow-x-hidden pb-8 lg:h-64">
+    <section className="bg-black w-full inline-flex flex-nowrap overflow-x-hidden pb-8 lg:h-64">
       <ul className="flex items-center space-x-4 lg:space-x-16 animate-loop-scroll px-0 lg:px-8 [&_li]:mx-4 [&_img]:max-w-none">
         {carouselBrands.map((item, i) => {
           return (
